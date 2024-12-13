5331a585f3613eea1a6b2cda7b00b5cad0fd4742
Semyon Levin <levin.semen@gmail.com>
2024-12-12 17:03:02 -0800

    [release-later] Additional tests

---
+++ src/test/java/name/remal/gradle_plugins/jacoco_to_cobertura/JacocoToCoberturaPluginTest.java
@@ -0,0 +1,40 @@
+package name.remal.gradle_plugins.jacoco_to_cobertura;
+
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.packageNameOf;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.unwrapGeneratedSubclass;
+import static name.remal.gradle_plugins.toolkit.testkit.ProjectValidations.executeAfterEvaluateActions;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import name.remal.gradle_plugins.toolkit.testkit.TaskValidations;
+import org.gradle.api.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@RequiredArgsConstructor
+class JacocoToCoberturaPluginTest {
+
+    final Project project;
+
+    @BeforeEach
+    void beforeEach() {
+        project.getPluginManager().apply(JacocoToCoberturaPlugin.class);
+    }
+
+    @Test
+    void pluginTasksDoNotHavePropertyProblems() {
+        project.getPluginManager().apply("java");
+        project.getPluginManager().apply("jacoco");
+
+        executeAfterEvaluateActions(project);
+
+        val taskClassNamePrefix = packageNameOf(JacocoToCoberturaPlugin.class) + '.';
+        project.getTasks().stream()
+            .filter(task -> {
+                val taskClass = unwrapGeneratedSubclass(task.getClass());
+                return taskClass.getName().startsWith(taskClassNamePrefix);
+            })
+            .forEach(TaskValidations::assertNoTaskPropertiesProblems);
+    }
+
+}
